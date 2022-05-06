809557526026b55559d0ffe8162496922e39e596
taisdn <105016362+taisdn@users.noreply.github.com>
2022-05-06 19:54:11 -0300

    Update cadastra_area.php

--- cadastra_area.php
+++ cadastra_area.php
@@ -5,7 +5,7 @@
     <html>
 
     <head>
-    <title>Sistema Empréstimo Livros</title>
+    <title> Empréstimo Livros</title>
     <link rel="stylesheet" type="text/css" href="estilo.css" />
     </head>
 
